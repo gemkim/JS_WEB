0230f0b5a7cde6ac4bb3a7077fa5c192969b3c56
JinSeokAn <dkswlstjr000@naver.com>
2023-05-07 23:57:30 +0900

    feat : WEB WAS 연동 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      react 예요..!!!
-    </div>
-  );
+    const [msg, setMsg] = useState([]);
+    useEffect(() => {
+        fetch("/api/hello")
+            .then((res) => {return res.json();})
+            .then((data) => {setMsg(data);})
+    }, []);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ul>
+                    {msg.map((content, idx) => <li key={`${idx} - ${content}`}>{content}</li>)}
+                </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;
