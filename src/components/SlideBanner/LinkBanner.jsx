3854cc0692d3141c1404c55d428a1061a35df314
gemkim <devgem91@gmail.com>
2023-05-10 23:04:39 +0900

    Refactor : - Swiper 라이브러리 추가 - slide 배너 컴포넌트 swiper로 리펙토링

---
+++ src/components/SlideBanner/LinkBanner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './Banner.module.scss'
+import "swiper/scss";
+import "swiper/css/navigation";
+
+const LinkBanner = ({key, imgURL}) => {
+  return  (
+    <div className={styles.linkBanner}>
+      <div className={styles.contextBox}>
+        <p className={styles.cate}>SWIM</p>
+        <p className={styles.title}>안입은듯 가벼운 초경량 <br />무게 답답함이 없는 완벽한 기능성</p>
+        <p className={styles.desc}>멀티 커버업 원피스 집업</p>
+      </div>
+      <Link className={styles.slide}><img src={require(`asset/${imgURL}`)} alt={`배너이미지 ${key}`} /></Link>
+    </div>
+  )
+};
+
+export default LinkBanner;
