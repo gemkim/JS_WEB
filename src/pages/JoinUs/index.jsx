b46be53c8b6479d2bc2b7297cfc3c20b73ffe50a
gemkim <devgem91@gmail.com>
2023-05-12 17:58:20 +0900

    Refactor : 로그인/회원가입 리펙토링

--- src/pages/JoinUs/index.jsx
+++ src/pages/JoinUs/index.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
 import styles from './JoinUs.module.scss'
 import InsertTextForm from 'components/InputForm/InsertTextForm';
+import Button from 'components/Button';
+import CheckBox from 'components/InputForm/CheckBox';
 
 const JoinUs = (props) => {
   return(
-    <div className={styles.login}>
+    <div className={styles.join}>
       <div className={styles.container}>
-        <h2>로그인</h2>
+        <h2>회원가입</h2>
         <div className={styles.loginArea}>
           <form>
-            <div className={styles.basicLogin}>
-              <InsertTextForm title={'아이디'} type={'text'} />
-              <InsertTextForm title={'비밀번호'} type={'password'} />
-              <InsertTextForm title={'비밀번호 확인'} type={'password'} />
-              <InsertTextForm title={'이름'} type={'text'} />
-              <InsertTextForm title={'핸드폰'} type={'text'} />
-              <InsertTextForm title={'여자'} type={'text'} />
-              <InsertTextForm title={'이메일'} type={'text'} />
-              <InsertTextForm title={'주소'} type={'text'} />
+            <div className={styles.formBox}>
+              <InsertTextForm guideTxt={'아이디'} type={'text'} />
+              <InsertTextForm guideTxt={'비밀번호'} type={'password'} />
+              <InsertTextForm guideTxt={'비밀번호 확인'} type={'password'} />
+              <InsertTextForm guideTxt={'이름'} type={'text'} />
+              <InsertTextForm guideTxt={'핸드폰'} type={'text'} />
+              {/* 젠더 */}
+              <div className={styles.gender}>
+                <CheckBox val={'check1'} text={'여자'} />
+                <CheckBox val={'check2'} text={'남자'} />
+              </div>
+              {/* select box */}
+              <div className={styles.emailform}>
+                <InsertTextForm guideTxt={'이메일'} type={'text'} />
+                <select name="" id="">
+                  <option value="naver.com">naver.com</option>
+                  <option value="gmail.com">gmail.com</option>
+                  <option value="hanmail.com">hanmail.com</option>
+                  <option value="직접입력">직접입력</option>
+                </select>
+              </div>
+              {/* 주소인증 */}
+              <div className={styles.address}>
+                <InsertTextForm guideTxt={'주소'} type={'text'} />
+                <Button type={'button'} text={'주소'} size={'btnS'} state={'white'} />
+              </div>
             </div>
-
-            <div>
+            {/* 동의하기 */}
+            <div className={styles.agreeWrap}>
               <h3>전체 동의</h3>
-              <div>
+              <div className={styles.agreeContent}>
                 동의하기 체크
               </div>
             </div>
 
             <div className={styles.btnWrap}>
-              <button type='button'>취소</button>
-              <button type='submit'>확인</button>
+              <Button type={'button'} text={'취소'} size={'btnL'} state={'cancel'} />
+              <Button type={'submit'} text={'확인'}  size={'btnL'} state={'success'} />
             </div>
           </form>
         </div>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from './Login.module.scss'
 // import InsertTextForm from 'components/InputForm/InsertTextForm';
 import { Link } from 'react-router-dom';
+import InsertTextForm from 'components/InputForm/InsertTextForm';
+import CheckBox from 'components/InputForm/CheckBox';
+import Button from 'components/Button';
 
 const Login = (props) => {
   return(
@@ -10,18 +13,23 @@ const Login = (props) => {
         <h2>로그인</h2>
         <div className={styles.loginArea}>
           <form>
-            <div className={styles.basicLogin}>
-              <input className={styles.formText} type="text" placeholder='아이디' />
-              <input className={styles.formText} type="password" placeholder='패스워드' />
-              <label className={styles.savedInfo} htmlFor="chkSavedId"><input id='chkSavedId' type="checkbox" />아이디 저장</label>
-              <button className={styles.btnLogin} type='button'>로그인하기</button>
+            {/* 기본 로그인 */}
+            <div className={styles.formBox}>
+              <InsertTextForm type={'text'} guideTxt={'아이디'} />
+              <InsertTextForm type={'password'} guideTxt={'패스워드'} />
+              <div className={styles.savedInfo}>
+                <CheckBox text={'아이디 저장'} />
+              </div>
+             <div className={styles.btnWrap}>
+                <Button type={'button'} text={'로그인'} size={'btnL'} state={'success'} />
+             </div>
               <div className={styles.joinfind}>
                 <Link to="">아이디/비밀번호 찾기</Link>
                 <Link to="/join-us">회원가입</Link>
               </div>
             </div>
-       
-            <div className={styles.socialArea}>
+            {/* SNS 인증로그인 */}
+            <div className={styles.formBox}>
               <h3>SNS 로그인 인증</h3>
               <ul className={styles.socialLogin}>
                 <li>
