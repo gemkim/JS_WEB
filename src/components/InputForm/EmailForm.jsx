95578f2b4939c66dea7c693199df1c204b9d7b29
gemkim <devgem91@gmai.com>
2023-05-19 00:00:33 +0900

    Refactor : 이메일 form 리팩토리 수정

--- src/components/InputForm/EmailForm.jsx
+++ src/components/InputForm/EmailForm.jsx
@@ -1,40 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Form.module.scss'
 
 const EmailForm = (props) => {
   // 초기값 세팅 - 이메일
   const [email, setEmail] = useState('');
-  // 오류메세지 상태 저장
-  const [emailMessage, setEmailMessage] = useState('');
-  // 유효성 검사
-  const [isEmail, setIsEmail] = useState(true)
+  const [selectValue, setSelectValue ] = useState('')
+  const [emailAddress, setEmailAddress] = useState('');
+ 
+  const [emailMessage, setEmailMessage] = useState(''); // 오류메세지 상태 저장
+  const [isEmail, setIsEmail] = useState(true);   // 유효성 검사
 
+  // 내용이 변경될 때
+  const onChangeEmailAddress = (e) => {
+    const currentEmail = e.target.value;
+    setSelectValue(currentEmail)
+  }
   const onChangeUserMail = (e) => {
     const currentEmail = e.target.value;
+    setEmail(currentEmail)
+  };
+
+  useEffect(()=>{
+    const total = `${email}@${selectValue}`;
+    setEmailAddress(total)
     const emailRegExp = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    setEmail(currentEmail);
-    if (!emailRegExp.test(currentEmail)) {
+    if (!emailRegExp.test(total)) {
       setEmailMessage("이메일의 형식이 올바르지 않습니다!");
       setIsEmail(false)
     } else {
       setEmailMessage("사용 가능한 이메일 입니다.");
       setIsEmail(true)
     }
-  };
+  }, [email, selectValue])
 
   return (
     <>
       <input type="text" placeholder='이메일' onChange={onChangeUserMail} />
-      { email.length > 0 && (
-        <p className={ `${styles.warningMsg} ${ isEmail ? styles.success : styles.error }` }>{emailMessage}</p>
-      )}
       <span className={styles.at}>@</span>
-      <select className={styles.emailLists} name="" id="">
+      <select name="" id="" onChange={onChangeEmailAddress}>
+        <option value="">이메일 선택하세요</option>
         <option value="naver.com">naver.com</option>
         <option value="gmail.com">gmail.com</option>
         <option value="hanmail.com">hanmail.com</option>
-        <option value="직접입력">직접입력</option>
+        <option value="userInsert">직접입력</option>
       </select>
+      { email.length > 0 && (
+        <p className={ `${styles.warningMsg} ${ isEmail ? styles.success : styles.error }` }>{emailMessage}</p>
+      )}
     </>
   )
 };
