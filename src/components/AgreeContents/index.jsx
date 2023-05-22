8c30f960ca8f2e8c64dac051a2198a2b74de5bc1
gemkim <devgem91@gmail.com>
2023-05-22 17:48:50 +0900

    Refactor : 전체 동의 기능 수정

--- src/components/AgreeContents/index.jsx
+++ src/components/AgreeContents/index.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, {  useCallback, useEffect, useState } from 'react'
 import styles from './AgreeContents.module.scss'
 
 const AgreeContents = ({agreeData}) => {
+  const [ checkAll, setCheckAll ] = useState(false); // 전체 동의확인 여부
+  const [ isChecked, setIsChecked ] = useState({ 
+    termsOfService: false,
+    privacyPolicy: false,
+    allowPromotions: false
+  })
+
+  const handleCheckAllChange = (e) => {
+    const { checked } = e.target;
+    setIsChecked((prev) => ({
+      termsOfService: checked,
+      privacyPolicy: checked,
+      allowPromotions: checked
+    }));
+  };
+
+  const handleChecked = (e) => {
+    const { name, checked } = e.target;
+    setIsChecked((prev) => (
+      { ...prev, [name]: checked }
+    ));
+  };
+  
+  const isAllChecked = Object.values(isChecked).every((value) => value);
+
   return (
     <div className={styles.contents}>
       <div className={styles.termsCheckAll}>
-        <input type="checkbox" name="checkAll" id="checkAll" />
-        <label for="checkAll">개인정보 수집 및 이용, 프로모션 정보 수신(선택)에 모두 동의합니다.</label>
+        <input 
+          type="checkbox" 
+          name="checkAll" 
+          id="checkAll" 
+          checked={isAllChecked}
+          onChange={ handleCheckAllChange }
+         />
+        <label htmlFor="checkAll">개인정보 수집 및 이용, 프로모션 정보 수신(선택)에 모두 동의합니다.</label>
       </div>
       <ul className={styles.termsList}>
         {
           agreeData.map( ({id, subject, contents, option, necessary}) => (
             <li key={id} className={styles.termsBox}>
               <div className={styles.inputCheck}>
-                <input type="checkbox" name="agreement" id={id} value={id} required />
-                <label for={id} className={styles.required}>
+                <input type="checkbox" name={id} id={id} value={id} checked={isChecked[id]} onChange={handleChecked} required />
+                <label htmlFor={id} className={styles.required}>
                   <p className={styles.termsSubject}>{subject}</p>
                   { option && <span className={ `${styles.option} ${styles.extra}` }>(선택)</span> }
                   { necessary && <span className={ `${styles.necessary} ${styles.extra}` }>(필수)</span> }
--- src/components/InputForm/AddressForm.jsx
+++ src/components/InputForm/AddressForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Form.module.scss'
 import Button from 'components/Button';
 
@@ -7,24 +7,26 @@ const AddressForm = ({isPopup, setIsPopup, address, zoneCode}) => {
     setIsPopup(!isPopup)
   }
 
-  const onChangeUserAddress = (e) => {};
+  const onChangeUserAddress = (e) => {
+    const target = e.target.value
+  };
   
   return (
     <>
       {/* userAddress */}
      <div className={styles.zonecode}>
-        <input type="text" value={zoneCode} />
+        <input type="text" defaultValue={zoneCode} disabled />
         <Button type={'button'} text={'주소'} size={'btnM'} state={'white'} onClick={handleOpenPopup} />
      </div>
      <input 
-          className={styles.textform} 
-          id="input1" 
-          type='text' 
-          placeholder='주소'
-          value={address}
-          onChange={onChangeUserAddress}
-        />
-        <input className={styles.textform}  type="text" placeholder='나머지 주소' />
+        className={styles.textform} 
+        id="input1" 
+        type='text' 
+        placeholder='주소'
+        value={address}
+        onChange={onChangeUserAddress}
+      />
+      <input className={styles.textform}  type="text" placeholder='나머지 주소' />
     </>
   )
 };
