814465a19a00a49f0afa9e86342780424449bf46
gemkim <devgem91@gmai.com>
2023-06-05 00:06:19 +0900

    Refactor : 이메일 컴포넌트 주문폼/회원가입 동기화 로직 재수정

--- src/components/InputForm/EmailForm.jsx
+++ src/components/InputForm/EmailForm.jsx
@@ -1,79 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Form.module.scss'
 
-const EmailForm = ({setFormData}) => {
+const EmailForm = ({setFormData, email = '@'}) => {
   // 초기값 세팅 - 이메일
-  const [emailForm, setEmailForm] = useState({
-    email: '',
-    emailOption: '',
-    emailTotal: '',
-  });
-  const {email, emailOption, emailTotal} = emailForm
+  const propsEmail = email.split('@'); // props 받아서 메일 구분
+
+  const [emailFront, setEmailFront] = useState(propsEmail[0] || '')
+  const [emailBack, setEmailBack] = useState(propsEmail[1] || '')
+
   const [emailMessage, setEmailMessage] = useState(''); // 오류메세지 상태 저장
   const [isEmail, setIsEmail] = useState(true);   // 유효성 검사
 
   // 내용이 변경될 때
-  const onChangeUserMail = (e) => updateFormData(e)
-  const onChangOption = (e) => updateFormData(e)
-
-  const updateFormData = (e) => {
-    const target = e.target
-    const currentValue = target.value;
-    setEmailForm((prevData) => ({
-      ...prevData,
-      [target.name]: currentValue
-    }));
+  const onChangeEmailFront = (e) => {
+    const currentValue = e.target.value;
+    setEmailFront(currentValue)
   }
- 
-  useEffect(()=>{
-    const total = `${email}@${emailOption}`;
-    setEmailForm((prevData) => ({
-      ...prevData,
-      emailTotal: total
-    }))
+  const onChangeEmailBack = (e) => {
+    const currentValue = e.target.value;
+    setEmailBack(currentValue)
+  }
 
-    const emailRegExp = /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    if (!emailRegExp.test(emailTotal)) {
+  useEffect(()=>{
+    let total = emailFront + '@' + emailBack; // 풀메일 주소
+    setFormData((prevData) => ({
+      ...prevData, email: total
+    }));
+
+    const emailRegExp = /([a-zA-z0-9]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (!emailRegExp.test(total)) {
       setEmailMessage("이메일의 형식이 올바르지 않습니다!");
       setIsEmail(false)
     } else {
       setEmailMessage("사용 가능한 이메일 입니다.");
       setIsEmail(true)
     }
-    setFormData((prevData) => ({
-      ...prevData,
-      email: total, 
-    }));
-
-  }, [email, emailOption, emailTotal])
+  }, [emailFront, emailBack])
 
   return (
-    <>
+    <div className={styles.emailform}>
       <input
         className={styles.inputEmail} 
         name='email'
         type="text" 
         placeholder='이메일' 
+        value={emailFront || ''}
         onChange={ e => {
-          onChangeUserMail(e)
+          onChangeEmailFront(e)
         }}
       />
       <span className={styles.at}>@</span>
       <select 
         className={styles.selectEmail} 
         name="emailOption" id="" 
+        value={emailBack}
         onChange={ e => {
-          onChangOption(e)
+          onChangeEmailBack(e)
         }}>
         <option value="">이메일 선택하세요</option>
         <option value="naver.com">naver.com</option>
         <option value="gmail.com">gmail.com</option>
         <option value="hanmail.com">hanmail.com</option>
       </select>
-      { email.length > 0 && (
+      { emailFront.length > 0 && (
         <p className={ `${styles.warningMsg} ${ isEmail ? styles.success : styles.error }` }>{emailMessage}</p>
       )}
-    </>
+    </div>
   )
 };
 
