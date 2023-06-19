5dfac2ea1b7ab53f6ddd2dc48eaf43b6672d8eed
gemkim <devgem91@gmail.com>
2023-06-19 17:53:44 +0900

    style : 코드 정리

--- src/components/SelectOptions/index.jsx
+++ src/components/SelectOptions/index.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const SelectOptions = ({title, data}) => {
   return(
     <select title={title} >
-      <option value="">선택해주세요.</option>
+      {/* <option value="">선택해주세요.</option> */}
       {
         data.map( list => (
-          <option value={list.val}>{list.name} {list.ect}</option>
+          <option key={list.id} value={list.val}>{list.name} {list.ect}</option>
         ))
       }
     </select>
--- src/components/order/OptionList.jsx
+++ src/components/order/OptionList.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './TableFormLists.module.scss'
 
 const OptionList = ({item, idx}) => {
   idx = idx + 1
   const optionTotalPrice = item.price * item.count   // 옵션 종류 별 합계
-
   return (
     <p>
       {idx}_{item.name} 
