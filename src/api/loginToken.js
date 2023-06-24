c34ee53b106f07a4e9b2bbae8c69e74cb9cd8a69
gemkim <devgem91@gmai.com>
2023-06-24 13:03:13 +0900

    Comment : jwt 참조 사이트 주석추가

--- src/api/loginToken.js
+++ src/api/loginToken.js
@@ -35,3 +35,6 @@ const addTokenToHeader = (token, header) => {
 };
 
 // https://velog.io/@h-young/%EB%A6%AC%EC%95%A1%ED%8A%B8-%EB%A1%9C%EA%B7%B8%EC%9D%B8-jwt%EB%B0%A9%EC%8B%9D
+//https://han-um.tistory.com/17
+// https://www.daleseo.com/js-jwt/
+// https://www.youtube.com/watch?v=nI8PYZNFtac
